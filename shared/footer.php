25c6523bbcd5a387715c7cb3d20bcff5b0a44191
LucasPrietoAlmeida <lucasprietoalmeidaan@gmail.com>
2025-09-18 13:25:29 +0200

    Create folder structure

---
+++ shared/footer.php
@@ -0,0 +1,5 @@
+<footer class="bg-light text-center text-lg-start mt-auto">
+  <div class="text-center p-3" style="background-color: #f8f9fa;">
+    © <?php echo date("Y"); ?> PlexSalud. Todos los derechos reservados.
+  </div>
+</footer>
---
+++ shared/header.php
@@ -0,0 +1,10 @@
+<header>
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+        <a class="navbar-brand" href="#">PlexSalud</a>
+        <div class="d-flex" >
+            <button class="btn btn-outline-danger" type="button">Cerrar sesión</button>
+        </div>
+        </div>
+    </nav>
+</header>
